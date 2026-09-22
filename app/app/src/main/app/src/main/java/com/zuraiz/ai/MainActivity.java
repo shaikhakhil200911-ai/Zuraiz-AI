0822680facdfff06af5b3c7217c3a9f702924e7f
shaikhakhil200911-ai <shaikhakhil200911@gmail.com>
2026-09-23 01:37:02 +0530

    Create MainActivity.java

---
+++ app/app/src/main/app/src/main/java/com/zuraiz/ai/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zuraiz.ai;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.welcomeText);
+        textView.setText("Zuraiz AI is Online");
+    }
+}
